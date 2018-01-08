f636cf1843e23c09ee1bdc536694b10d07bdfbc7
Sebastian Bossarte <kommentgames@gmail.com>
2018-01-08 10:42:11 -0600

    Created initial bare plugin.

---
+++ src/DefaultSite.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Default Site plugin for Craft 3.0
+ * @copyright Copyright Charlie Development
+ */
+
+namespace charliedev\defaultsite;
+
+use Craft;
+use craft\base\Plugin;
+use craft\services\Fields;
+
+/**
+ * The main Craft plugin class.
+ */
+class DefaultSite extends Plugin
+{
+
+	/**
+	 * @inheritdoc
+	 * @see craft\base\Plugin
+	 */
+	public function init()
+	{
+		parent::init();
+	}
+}
